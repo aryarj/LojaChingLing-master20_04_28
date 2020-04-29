7a53f412a05e69df525cd575e31bda544bc446cf
Ary de Araujo Rodrigues Junior <aryarjyy@yahoo.com.br>
2020-04-28 22:57:51 -0300

    Add files via upload

---
+++ LojaChingLing-master20_04_28/FrmPedido.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace LojaCL
+{
+    partial class FrmPedido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnEcluirItem = new System.Windows.Forms.Button();
+            this.btnEditarItem = new System.Windows.Forms.Button();
+            this.btnNovoItem = new System.Windows.Forms.Button();
+            this.txtValorTotal = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.cbxProduto = new System.Windows.Forms.ComboBox();
+            this.lblValorTotal = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.dgvPedido = new System.Windows.Forms.DataGridView();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.lblCartao = new System.Windows.Forms.Label();
+            this.cbxCartao = new System.Windows.Forms.ComboBox();
+            this.btnPedido = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblIdProduto = new System.Windows.Forms.Label();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedido)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(250, 108);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 35;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
+            // 
+            // btnEcluirItem
+            // 
+            this.btnEcluirItem.Location = new System.Drawing.Point(394, 147);
+            this.btnEcluirItem.Name = "btnEcluirItem";
+            this.btnEcluirItem.Size = new System.Drawing.Size(75, 23);
+            this.btnEcluirItem.TabIndex = 32;
+            this.btnEcluirItem.Text = "Excluir Item";
+            this.btnEcluirItem.UseVisualStyleBackColor = true;
+            this.btnEcluirItem.Click += new System.EventHandler(this.btnEcluirItem_Click);
+            // 
+            // btnEditarItem
+            // 
+            this.btnEditarItem.Location = new System.Drawing.Point(302, 147);
+            this.btnEditarItem.Name = "btnEditarItem";
+            this.btnEditarItem.Size = new System.Drawing.Size(75, 23);
+            this.btnEditarItem.TabIndex = 31;
+            this.btnEditarItem.Text = "Editar Item";
+            this.btnEditarItem.UseVisualStyleBackColor = true;
+            this.btnEditarItem.Click += new System.EventHandler(this.btnEditarItem_Click);
+            // 
+            // btnNovoItem
+            // 
+            this.btnNovoItem.Location = new System.Drawing.Point(212, 147);
+            this.btnNovoItem.Name = "btnNovoItem";
+            this.btnNovoItem.Size = new System.Drawing.Size(75, 23);
+            this.btnNovoItem.TabIndex = 30;
+            this.btnNovoItem.Text = "Novo Item";
+            this.btnNovoItem.UseVisualStyleBackColor = true;
+            this.btnNovoItem.Click += new System.EventHandler(this.btnNovoItem_Click);
+            // 
+            // txtValorTotal
+            // 
+            this.txtValorTotal.Enabled = false;
+            this.txtValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValorTotal.Location = new System.Drawing.Point(369, 317);
+            this.txtValorTotal.Name = "txtValorTotal";
+            this.txtValorTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtValorTotal.TabIndex = 29;
+            this.txtValorTotal.TextChanged += new System.EventHandler(this.txtValorTotal_TextChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Enabled = false;
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(83, 140);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 28;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            // 
+            // cbxProduto
+            // 
+            this.cbxProduto.FormattingEnabled = true;
+            this.cbxProduto.Location = new System.Drawing.Point(83, 73);
+            this.cbxProduto.Name = "cbxProduto";
+            this.cbxProduto.Size = new System.Drawing.Size(279, 21);
+            this.cbxProduto.TabIndex = 27;
+            this.cbxProduto.SelectedIndexChanged += new System.EventHandler(this.cbxProduto_SelectedIndexChanged);
+            // 
+            // lblValorTotal
+            // 
+            this.lblValorTotal.AutoSize = true;
+            this.lblValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorTotal.Location = new System.Drawing.Point(293, 320);
+            this.lblValorTotal.Name = "lblValorTotal";
+            this.lblValorTotal.Size = new System.Drawing.Size(69, 13);
+            this.lblValorTotal.TabIndex = 26;
+            this.lblValorTotal.Text = "Valor Total";
+            this.lblValorTotal.Click += new System.EventHandler(this.lblValorTotal_Click);
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(7, 147);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(70, 13);
+            this.lblValor.TabIndex = 25;
+            this.lblValor.Text = "Valor Unitario";
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(15, 115);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(62, 13);
+            this.lblQuantidade.TabIndex = 24;
+            this.lblQuantidade.Text = "Quantidade";
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Location = new System.Drawing.Point(15, 77);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(44, 13);
+            this.lblProduto.TabIndex = 23;
+            this.lblProduto.Text = "Produto";
+            // 
+            // dgvPedido
+            // 
+            this.dgvPedido.AllowUserToAddRows = false;
+            this.dgvPedido.AllowUserToDeleteRows = false;
+            this.dgvPedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPedido.Location = new System.Drawing.Point(18, 183);
+            this.dgvPedido.Name = "dgvPedido";
+            this.dgvPedido.ReadOnly = true;
+            this.dgvPedido.Size = new System.Drawing.Size(451, 125);
+            this.dgvPedido.TabIndex = 22;
+            this.dgvPedido.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPedido_CellClick);
+            this.dgvPedido.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVenda_CellContentClick);
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(83, 108);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantidade.TabIndex = 21;
+            this.txtQuantidade.TextChanged += new System.EventHandler(this.txtQuantidade_TextChanged);
+            // 
+            // lblCartao
+            // 
+            this.lblCartao.AutoSize = true;
+            this.lblCartao.Location = new System.Drawing.Point(15, 19);
+            this.lblCartao.Name = "lblCartao";
+            this.lblCartao.Size = new System.Drawing.Size(35, 13);
+            this.lblCartao.TabIndex = 20;
+            this.lblCartao.Text = "Catão";
+            // 
+            // cbxCartao
+            // 
+            this.cbxCartao.FormattingEnabled = true;
+            this.cbxCartao.Location = new System.Drawing.Point(83, 19);
+            this.cbxCartao.Name = "cbxCartao";
+            this.cbxCartao.Size = new System.Drawing.Size(191, 21);
+            this.cbxCartao.TabIndex = 19;
+            this.cbxCartao.SelectedIndexChanged += new System.EventHandler(this.cbxCartao_SelectedIndexChanged);
+            // 
+            // btnPedido
+            // 
+            this.btnPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPedido.Location = new System.Drawing.Point(302, 17);
+            this.btnPedido.Name = "btnPedido";
+            this.btnPedido.Size = new System.Drawing.Size(92, 23);
+            this.btnPedido.TabIndex = 18;
+            this.btnPedido.Text = "Abrir Pedido";
+            this.btnPedido.UseVisualStyleBackColor = true;
+            this.btnPedido.Click += new System.EventHandler(this.btnPedido_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(15, 50);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 37;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Enabled = false;
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(83, 47);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(180, 20);
+            this.txtUsuario.TabIndex = 36;
+            // 
+            // lblIdProduto
+            // 
+            this.lblIdProduto.AutoSize = true;
+            this.lblIdProduto.Location = new System.Drawing.Point(226, 111);
+            this.lblIdProduto.Name = "lblIdProduto";
+            this.lblIdProduto.Size = new System.Drawing.Size(18, 13);
+            this.lblIdProduto.TabIndex = 38;
+            this.lblIdProduto.Text = "ID";
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(242, 356);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(135, 23);
+            this.btnAtualizar.TabIndex = 40;
+            this.btnAtualizar.Text = "Atualizar Pedido";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizar.Location = new System.Drawing.Point(64, 356);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(161, 23);
+            this.btnFinalizar.TabIndex = 41;
+            this.btnFinalizar.Text = "Finalizar Pedido";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click_1);
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocalizar.Location = new System.Drawing.Point(400, 17);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(92, 23);
+            this.btnLocalizar.TabIndex = 42;
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // FrmPedido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 391);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblIdProduto);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnEcluirItem);
+            this.Controls.Add(this.btnEditarItem);
+            this.Controls.Add(this.btnNovoItem);
+            this.Controls.Add(this.txtValorTotal);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.cbxProduto);
+            this.Controls.Add(this.lblValorTotal);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.dgvPedido);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.lblCartao);
+            this.Controls.Add(this.cbxCartao);
+            this.Controls.Add(this.btnPedido);
+            this.Name = "FrmPedido";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pedido";
+            this.Load += new System.EventHandler(this.FrmPedido_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedido)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnEcluirItem;
+        private System.Windows.Forms.Button btnEditarItem;
+        private System.Windows.Forms.Button btnNovoItem;
+        private System.Windows.Forms.TextBox txtValorTotal;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.ComboBox cbxProduto;
+        private System.Windows.Forms.Label lblValorTotal;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.DataGridView dgvPedido;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label lblCartao;
+        private System.Windows.Forms.ComboBox cbxCartao;
+        private System.Windows.Forms.Button btnPedido;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblIdProduto;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Button btnLocalizar;
+    }
+}
